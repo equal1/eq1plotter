c51a1be5d34691d6d78868932a9074ddd1a9364f
Dennis Andrade-Miceli <dandrademiceli@gmail.com>
2020-04-06 17:16:23 +0100

    update

---
+++ eq1plotter.py
@@ -0,0 +1,18 @@
+
+
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import tkinter as tk
+
+print('''
+        ***************************
+        eq1plotter v0.0
+        Equal1 Labs
+        M. Asker, D. Andrade-Miceli
+        2020
+        ***************************
+        ''')
+
+
