cf1c3f3f71612b084fd234d05257940a7c738a05
Dennis Andrade-Miceli <dandrademiceli@gmail.com>
2020-04-06 18:55:49 +0100

    update

--- eq1plotter.py
+++ eq1plotter.py
@@ -6,8 +6,11 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import tkinter as tk
 import tkinter.tix as tix
-import re, os, system, csv, getopt, math, commands, csv, types
+import re, os, sys, csv, getopt, math, csv, types, gc
 import glob, time, shutil, getpass, subprocess, traceback
+from datetime import datetime
+from pprint import pprint
+
 
 print('''
         ***************************
